cf244c4e09dc45c1839a50ebc5c401cbb84deaf1
Dao Quoc Viet <quocvietdao2705@gmail.com>
2026-07-06 16:23:39 +0700

    ref : decks sort logic

--- src/services/deck.service.js
+++ src/services/deck.service.js
@@ -13,7 +13,19 @@ class DeckService {
    * Có thể tuỳ biến logic nếu backend cho phép tìm theo creator_id hoặc public status.
    */
   async getAll(params = {}) {
-    return (await this.api.get("/", { params })).data;
+    const data = (await this.api.get("/", { params })).data;
+    
+    // Sort array by lesson (e.g. Lesson 1, Lesson 2, Lesson 10...)
+    if (Array.isArray(data)) {
+      data.sort((a, b) => {
+        if (a.title && b.title) {
+          return a.title.localeCompare(b.title, undefined, { numeric: true, sensitivity: 'base' });
+        }
+        return 0;
+      });
+    }
+    
+    return data;
   }
 
   async create(data) {
